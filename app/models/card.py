d5c27bfa21dd700965670df9d9120ba2cbd0d441
Jeslyn Lai <imlyj5@gmail.com>
2025-06-23 11:54:00 -0700

    fix bug

--- app/models/card.py
+++ app/models/card.py
@@ -1,12 +1,12 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import ForeignKey
 from .base import Base
 
 class Card(Base):
     __tablename__ = "card"
 
     card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    message: Mapped[str] = mapped_column(String)
-    likes_count: Mapped[int] = mapped_column(Integer)
+    message: Mapped[str]
+    likes_count: Mapped[int]
     board_id: Mapped[int] = mapped_column(ForeignKey("board.board_id"))
     board: Mapped["Board"] = relationship(back_populates="cards")
